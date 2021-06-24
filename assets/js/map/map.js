956f6c5a469361f56a2a97041af4bad8075b42de
Mishieck <mishieckmwale@gmail.com>
2021-06-24 15:20:52 +0200

    feat: change zoom and center of map

--- assets/js/map/map.js
+++ assets/js/map/map.js
@@ -1,34 +1,29 @@
 import { popup } from "./popup.js";
 
-
-const LUSAKA_COORDS = [-15.40669, 28.28713];
+const LUSAKA_COORDS = [-15.38979694, 28.31819683];
 const MAX_ZOOM = 19;
 
 const LAYER_URLS = {
   streets: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 };
 
-let map,
-    streets,
-    marker,
-    circle,
-    MarkerIcon;
+let map, streets, marker, circle, MarkerIcon;
 
 const initMap = async () => {
-  map = L.map('map').setView(LUSAKA_COORDS, 16);
-  $('.leaflet-control-attribution' ).css('display', 'none');
+  map = L.map("map").setView(LUSAKA_COORDS, 14);
+  $(".leaflet-control-attribution").css("display", "none");
 
   streets = L.tileLayer(LAYER_URLS.streets, { maxZoom: MAX_ZOOM });
   streets.addTo(map);
-  
+
   marker = L.marker();
   circle = L.circle();
 
-  // Map controls positioning 
+  // Map controls positioning
   await addControlPlaceholders(map);
 
   // Change the position of the Zoom Control to a newly created placeholder.
-  map.zoomControl.setPosition('verticalcenterright');
+  map.zoomControl.setPosition("verticalcenterright");
 
   MarkerIcon = await MarkerIconClass();
   await addOverlays();
@@ -38,9 +33,9 @@ const initMap = async () => {
 const MarkerIconClass = async () => {
   return L.Icon.extend({
     options: {
-      iconSize:     [48, 48],
-      iconAnchor:   [24, 48],
-      popupAnchor:  [0, -48]
+      iconSize: [48, 48],
+      iconAnchor: [24, 48],
+      popupAnchor: [0, -48]
     }
   });
 };
@@ -50,15 +45,15 @@ const addOverlays = async () => {
   L.geoJSON(overlays.features, { onEachFeature });
 };
 
-const getOverlays = async () => (await fetch('/assets/json/main.json')).json();
+const getOverlays = async () => (await fetch("/assets/json/main.json")).json();
 
 const onEachFeature = async (feature, layer) => {
   const passability = feature.properties.passability;
 
   const markerURLs = {
-    "Closed": "/assets/img/icons/road-signs/red-marker.svg",
+    Closed: "/assets/img/icons/road-signs/red-marker.svg",
     "Partially Closed": "/assets/img/icons/road-signs/yellow-marker.svg",
-    "Open": "/assets/img/icons/road-signs/green-marker.svg"
+    Open: "/assets/img/icons/road-signs/green-marker.svg"
   };
 
   const markerURL = markerURLs[passability];
@@ -67,7 +62,7 @@ const onEachFeature = async (feature, layer) => {
 
   const html = await popup(feature);
   layer.bindPopup(html);
-  layer.on('click', async e => {
+  layer.on("click", async (e) => {
     layer.openPopup();
     siteImageClick();
   });
@@ -76,39 +71,33 @@ const onEachFeature = async (feature, layer) => {
 // Create additional Control placeholders
 const addControlPlaceholders = async (map) => {
   const corners = map._controlCorners,
-        l = 'leaflet-',
-        container = map._controlContainer;
+    l = "leaflet-",
+    container = map._controlContainer;
 
-  createCorner('verticalcenter', 'left');
-  createCorner('verticalcenter', 'right');
+  createCorner("verticalcenter", "left");
+  createCorner("verticalcenter", "right");
 
   function createCorner(vSide, hSide) {
-    const className = l + vSide + ' ' + l + hSide;
-    corners[vSide + hSide] = L.DomUtil.create('div', className, container);
-  };
+    const className = l + vSide + " " + l + hSide;
+    corners[vSide + hSide] = L.DomUtil.create("div", className, container);
+  }
 };
 
 const locateUser = async () => {
-  const locateMarkerIcon = new MarkerIcon({iconUrl: '/assets/images/icons/road-signs/crosshair-marker.svg'});
+  const locateMarkerIcon = new MarkerIcon({ iconUrl: "/assets/images/icons/road-signs/crosshair-marker.svg" });
 
-  map.locate({ setView: true, watch: true })
-  .on('locationfound', (e) => {
+  map.locate({ setView: true, watch: true }).on("locationfound", (e) => {
     map.removeLayer(marker);
     marker = L.marker(e.latlng).setIcon(locateMarkerIcon).addTo(map);
 
-    circle = L.circle(
-      e.latlng, 
-      e.accuracy/2,
-      {
-        weight: 1,
-        color: 'blue',
-        fillColor: '#cacaca',
-        fillOpacity: 0.2
-      }
-    )
-    .addTo(map);
+    circle = L.circle(e.latlng, e.accuracy / 2, {
+      weight: 1,
+      color: "blue",
+      fillColor: "#cacaca",
+      fillOpacity: 0.2
+    }).addTo(map);
 
-    map.setView([e.latitude, e.longitude], 16)
+    map.setView([e.latitude, e.longitude], 16);
   });
 };
 
@@ -118,8 +107,8 @@ const addEvents = async () => {
   //     circle.setRadius(currentZoom);
   // });
 
-  $('.basemap').on('change', (e) => switchBaseLayer(e.target.value));
-  $('#locate').on('click', () => locateUser());
+  $(".basemap").on("change", (e) => switchBaseLayer(e.target.value));
+  $("#locate").on("click", () => locateUser());
 };
 
 const siteImageClick = () => {
@@ -127,7 +116,7 @@ const siteImageClick = () => {
   $siteImageLinks.click(function (e) {
     showImageViewer(this);
   });
-}
+};
 
 const showImageViewer = (link) => {
   const links = link.parentElement.children;
@@ -138,7 +127,7 @@ const showImageViewer = (link) => {
   $imageContainer.append($images);
   $imageViewer.find(".modal-title").text(name);
   $imageViewer.modal("show");
-}
+};
 
 const createImage = (src, alt, i) => {
   const $carouselItem = $("<div />", {
@@ -155,5 +144,5 @@ const createImage = (src, alt, i) => {
 
   return $carouselItem;
 };
-  
+
 export { initMap };
